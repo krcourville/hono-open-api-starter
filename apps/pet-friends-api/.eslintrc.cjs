d07a55aa6b54e87ef3dcde79c7a5eb4da4fa5e4a
Ken Courville <ken.courville@dhigroupinc.com>
2025-01-20 05:13:15 -0700

    eliminate warning

--- apps/pet-friends-api/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["@repo/eslint-config/base"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
